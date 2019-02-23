53476cb7b9f8c23140d64be10a997df4d64f46ce
g.a. mercado <flick.making@gmail.com>
2019-02-23 19:29:16 -0400

    Update TFF_Token.sol

--- contracts/TFF_Token.sol
+++ contracts/TFF_Token.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-eth/contracts/token/ERC721/ERC721Metadata.sol";
-import "openzeppelin-eth/contracts/access/roles/MinterRole.sol";
-import "openzeppelin-eth/contracts/ownership/Ownable.sol";
+import "openzeppelin-eth/contracts/token/ERC721/ERC721MetadataMintable.sol";
 import "zos-lib/contracts/Initializable.sol";
 
 /**
@@ -12,7 +10,7 @@ import "zos-lib/contracts/Initializable.sol";
  * b) disabling trading during voting periods.
  */
 
-contract TFF_Token is Initializable, ERC721, ERC721Metadata, MinterRole, Ownable {
+contract TFF_Token is Initializable, ERC721MetadataMintable {
 
   address private Owner;
   string private Name;
@@ -28,12 +26,9 @@ contract TFF_Token is Initializable, ERC721, ERC721Metadata, MinterRole, Ownable
     Name = "TFF_Token";
     ERC721.initialize();
     ERC721Metadata.initialize(Name, Symbol);
-    Ownable.initialize(sender);
-    MinterRole.initialize(sender);
-    require(ERC721._hasBeenInitialized());
-    require(ERC721Metadata._hasBeenInitialized());
-    TokenURI = "http://thefaustflick.com/images/TFF_Token.png";
+    ERC721MetadataMintable.initialize(Owner);
     TokenId = 1;
+    TokenURI = "http://thefaustflick.com/images/TFF_Token.png";
     for (uint8 counter = 0; counter <= 3; counter++) { MintStage[counter] = 0; }
     TokensToMint[0] = 500000;
     TokensToMint[1] = 3000000;
@@ -47,15 +42,15 @@ contract TFF_Token is Initializable, ERC721, ERC721Metadata, MinterRole, Ownable
    * TokenId to figure out next logically correct Mint_TFF(_Stage) value
    */
   function Mint_TFF(uint8 _Stage) public onlyMinter returns (bool) {
-    require(MintStage[_Stage] == 0);
-    uint256 tokensToMint = TokensToMint[_Stage];
-    for (uint256 counter = 1; counter <= tokensToMint; counter++) {
-      _mint(Owner, TokenId);
-      _setTokenURI(TokenId, TokenURI);
-      TokenId = TokenId.add(1);
+    if (MintStage[_Stage] == 0) {
+      uint256 tokensToMint = TokensToMint[_Stage];
+      for (uint256 counter = 1; counter <= tokensToMint; counter++) {
+        mintWithTokenURI(Owner, TokenId, TokenURI);
+        TokenId = TokenId.add(1);
+      }
+      MintStage[_Stage] = 1;
+      return true;
     }
-    MintStage[_Stage] = 1;
-    return true;
   }
 
   /**
@@ -69,6 +64,4 @@ contract TFF_Token is Initializable, ERC721, ERC721Metadata, MinterRole, Ownable
   function GetTokenURI() public view returns (string memory) {
       return TokenURI;
   }
-
-  uint256[50] private ______gap;
 }
