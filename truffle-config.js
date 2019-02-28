46deeb14f9c467a3a89c6ced475dd7cc7ca48d6c
g.a <flick.making@gmail.com>
2019-02-28 10:35:57 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -2,6 +2,9 @@ const path = require("path");
 require('dotenv').config();
 const devmnem = process.env.DEVMNEN;
 const kovmnem = process.env.KOVMNEN;
+const ropmnem = process.env.ROPMNEN;
+const rinmnem = process.env.RINMNEN;
+const maimnem = process.env.MAIMNEN;
 const key = process.env.INFURA_API_KEY;
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
@@ -14,7 +17,7 @@ module.exports = {
     },
     network_id: 5777,
     gas: 10000000000,
-    gasPrice: 1000000000,
+    gasPrice: 1,
     },
     kovan: {
       provider: function() {
@@ -26,7 +29,7 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + key);
+        return new HDWalletProvider(ropmnem, 'https://ropsten.infura.io/v3/' + key);
     },
       network_id: 3,
       gas: 8000000,
@@ -34,7 +37,7 @@ module.exports = {
     },
     rinkeby: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/' + key);
+        return new HDWalletProvider(rinmnem, 'https://rinkeby.infura.io/v3/' + key);
     },
       network_id: 4,
       gas: 8000000,
@@ -42,11 +45,11 @@ module.exports = {
     },
     mainnet: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/v3' + key);
+        return new HDWalletProvider(maimnem, 'https://mainnet.infura.io/v3' + key);
     },
       network_id: 1,
-      gas: 8000000,
-      gasPrice: 11000000000,
+      gas: 5000000,
+      gasPrice: 10000000000,
     }
   }
 };
