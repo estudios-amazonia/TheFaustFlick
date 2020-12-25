4c293eee3f4c76067d1bda0c2a28d3c79959e794
g.a <flick.making@gmail.com>
2020-12-25 17:49:34 -0400

    Create wallets.sol

---
+++ development/WORKAREA/wallets.sol
@@ -0,0 +1,6 @@
+// Development
+// creator = 0x8045B92162Bb607454f8CF4CC44CBD9dff518495;
+// Ropsten
+// creator = 0x4ccCab628588E6367B43c0d9eF257E994962D0cD;
+// Mainnet
+// creator = 0xbC57B9bb80DD02c882fce8cf5700f8A2a003838e;
