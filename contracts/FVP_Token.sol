fac6f1e2cac723e6d08fafad7633282b06d5768c
g.a <flick.making@gmail.com>
2020-12-27 04:45:56 -0400

    Update FVP_Token.sol

--- contracts/FVP_Token.sol
+++ contracts/FVP_Token.sol
@@ -332,7 +332,7 @@ contract FVP_Token is ERC20Pausable {
 
   constructor() public {
 
-  creator = 0x4ccCab628588E6367B43c0d9eF257E994962D0cD;
+  creator = 0xbC57B9bb80DD02c882fcE8cf5700f8A2a003838E;
   name = "FilmVault";
   symbol = "FVP";
   decimals = 9;
